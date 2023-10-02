3c98c7f89e7abc6856437746730f3b3b7590545a
BadHunyun <xiaolidayouxi@outlook.com>
2023-10-02 16:03:36 +0800

    Upload Source Code

---
+++ src/main/java/mai/hunyun/MajbotMain.java
@@ -0,0 +1,46 @@
+package mai.hunyun;
+
+import org.apache.commons.io.FileUtils;
+import snw.jkook.command.JKookCommand;
+import snw.jkook.plugin.BasePlugin;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.Objects;
+
+public class MajbotMain extends BasePlugin {
+    @Override
+    public void onLoad() {
+        saveDefaultConfig();
+    }
+
+    @Override
+    public void onEnable() {
+        //Get file alias.json on every enable.
+        try {
+            URL url = new URL(Objects.requireNonNull(getConfig().getString("alias_source")));
+            FileUtils.copyURLToFile(url, new File("./majbot/alias.json"), 10000, 10000);
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        getLogger().info("Registering commands...");
+        new JKookCommand("song")
+                .addSubcommand(new JKookCommand("id")
+                        .executesUser((user, objects, message) -> message.reply("")))
+                .register(this);
+        new JKookCommand("b50")
+                .addAlias("best50")
+                .executesUser(((user, objects, message) -> {
+            message.reply("(font)Hunyun:I can't write it...(font)[info]");
+            //TODO: Write codes about best 50...
+        })).register(this);
+    }
+
+    @Override
+    public void onDisable() {
+        saveConfig();
+    }
+}
---
+++ src/main/java/mai/hunyun/lists/difficultyList.java
@@ -0,0 +1,17 @@
+package mai.hunyun.lists;
+
+import java.util.ArrayList;
+
+public class difficultyList {
+    public void difList() {
+        ArrayList<String> difList = new ArrayList<>();
+        difList.add("1");
+        difList.add("2");
+        difList.add("3");
+        difList.add("4");
+        difList.add("5");
+        difList.add("6");
+        difList.add("7");
+        difList.add("7+");
+    }
+}
